95f4d0621c8651bf3ad8d7cb24faf4b9b2aa6624
Prabal Poddar <prabalpoddar73@gmail.com>
2025-08-03 12:42:15 +0530

    Update global styles
    global bg

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -41,7 +41,7 @@ function ImageGallery() {
   };
 
   return (
-    <section className="py-16 bg-gray-50">
+    <section className="py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-12">
